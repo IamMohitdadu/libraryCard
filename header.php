ed30d8ad72ef886e1f7b66bbe334a2fe5e83fc8d
Mohit Dadu <Mohit Dadu>
2017-02-15 12:14:23 +0530

    removed database instance from config file and added to required fields

---
+++ header.php
@@ -0,0 +1,26 @@
+<?php 
+/*
+* file-name: header.php
+* created-by: Mohit Dadu
+* description: it is the php header file used for all the web pages.
+* date:03/02/2017
+*/
+?>
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      
+      <!-- Title for web page -->
+      <title><?php echo isset($pageTitle) ? $pageTitle : "Library Card"?></title>
+
+      <!-- Custom CSS File   -->
+      <link rel="stylesheet" type="text/css" href="./asset/css/main.css"/> 
+
+      <!-- Bootstrap CSS File  -->
+      <link rel="stylesheet" type="text/css" href="./asset/vendors/css/bootstrap.css"/>
+
+    </head>
+    <body>
+    
